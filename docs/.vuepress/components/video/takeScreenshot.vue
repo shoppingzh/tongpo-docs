f53c87249b914393691c99662de48359b8b390b2
shoppingzh <1049262057@qq.com>
2022-01-09 15:35:37 +0800

    update examples

--- docs/.vuepress/components/video/takeScreenshot.vue
+++ docs/.vuepress/components/video/takeScreenshot.vue
@@ -1,15 +1,55 @@
 <template>
   <div>
-    
+    <p>
+      <video
+        ref="video"
+        src="./1.mp4"
+        controls />
+    </p>
+    <p>
+      <a-button
+        type="primary"
+        @click="screenshot">视频截图</a-button>
+    </p>
+    <p v-if="image">
+      <img :src="image">
+    </p>
   </div>
 </template>
 
 <script>
-export default {
+import { ref } from '@vue/composition-api'
+import { takeScreenshot } from 'tongpo/lib/video'
 
+export default {
+  setup() {
+    const video = ref(null)
+    const image = ref(null)
+
+    const screenshot = async() => {
+      try {
+        const blob = await takeScreenshot(video.value)
+        image.value && URL.revokeObjectURL(image.value)
+        image.value = URL.createObjectURL(blob)
+      } catch (err) {
+        console.error(err)
+      }
+    }
+    return {
+      video,
+      image,
+
+      screenshot
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="stylus" scoped>
+  video {
+    width: 600px;
+    height: 300px;
+    object-fit: contain;
+    background-color: rgba(0, 0, 0, .85);
+  }
 </style>
