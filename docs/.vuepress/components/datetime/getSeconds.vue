be5246c2f2aef488c3dd2c51b7e17cab718a7fe8
shoppingzh <1049262057@qq.com>
2022-01-16 14:26:02 +0800

    update examples

--- docs/.vuepress/components/datetime/getSeconds.vue
+++ docs/.vuepress/components/datetime/getSeconds.vue
@@ -1,12 +1,39 @@
 <template>
   <div>
-    
+    <p>
+      <a-input
+        v-model="time"
+        placeholder="如 01:12:30" />
+    </p>
+    <p>
+      <a-button
+        type="primary"
+        @click="calc">计算</a-button>
+    </p>
+    <a-alert v-if="seconds != null" :message="seconds" />
   </div>
 </template>
 
 <script>
-export default {
+import { ref } from '@vue/composition-api'
+import { getSeconds } from 'tongpo/lib/datetime'
 
+export default {
+  setup() {
+    const time = ref('01:20:30')
+    const seconds = ref(null)
+    
+    const calc = () => {
+      seconds.value = getSeconds(time.value)
+    }
+
+    return {
+      time,
+      seconds,
+
+      calc
+    }
+  }
 }
 </script>
 
