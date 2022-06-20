5381373489b2eb2f97bb86643f644ef2212ccf96
郑晓平 <zhengxiaoping172@sf-express.com>
2022-06-20 14:09:47 +0800

    update config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,10 +2,14 @@ import { defineConfig } from 'vitepress'
 import { autoGenerateSidebar, getFirstDocLink } from 'press-util'
 
 export default defineConfig({
+
+  base: '/tongpo/',
+
   title: '乐天工具库',
   description: '竹杖芒鞋轻胜马，谁怕？一蓑烟雨任平生。',
 
   themeConfig: {
+    logo: '/logo.png',
     nav: [{
       text: '文档',
       link: getFirstDocLink('doc')
